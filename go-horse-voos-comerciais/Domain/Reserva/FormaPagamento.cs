eaf7ee25b9278746811cd543dd79802744c0d778
gPagio <guilhermepagio.contato@gmail.com>
2024-08-24 17:58:40 -0300

    feat: add forma de pagamento da reserva
    Cria as formas de pagamento para a reserva de voo

---
+++ go-horse-voos-comerciais/Domain/Reserva/FormaPagamento.cs
@@ -0,0 +1,6 @@
+public enum FormaPagamento 
+{
+    Cartao,
+    Transferencia,
+    Dinheiro
+}
--- go-horse-voos-comerciais/Domain/Reserva/Reserva.cs
+++ go-horse-voos-comerciais/Domain/Reserva/Reserva.cs
@@ -5,6 +5,7 @@ public class Reserva
     public long Id { get; set; }
     public long IdCliente { get; set; }
     public DateTime DataReserva { get; set; }
+    public FormaPagamento FormaPagamento{ get; set; }
     public long IdVoo { get; set; }
     public List<Passagem>? PassagemList { get; set;}
     public StatusReserva Status { get; set; }
