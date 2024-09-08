8786c48701c96502d8b3894c4fc4a9509a4e0621
gPagio <guilhermepagio.contato@gmail.com>
2024-09-08 19:19:24 -0300

    chore: arquivos de migrations

---
+++ go-horse-voos-comerciais/Migrations/20240907214230_Voos.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace go_horse_voos_comerciais.Migrations
+{
+    /// <inheritdoc />
+    public partial class Voos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "voos",
+                columns: table => new
+                {
+                    id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    id_origem = table.Column<long>(type: "bigint", nullable: false),
+                    id_destino = table.Column<long>(type: "bigint", nullable: false),
+                    data_ida = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    data_volta = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    id_companhia_operante = table.Column<long>(type: "bigint", nullable: false),
+                    preco = table.Column<double>(type: "double precision", nullable: false),
+                    quantiade_assentos = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_voos", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "voos");
+        }
+    }
+}
