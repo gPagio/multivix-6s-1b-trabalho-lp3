229bc36f823d5a05be0cb57566e40caaf0c35756
gPagio <guilhermepagio.contato@gmail.com>
2024-09-11 16:46:14 -0300

    feat: servico de passagens
    Cria o servico necessario para realizar operacoes com passagens

---
+++ go-horse-voos-comerciais/Domain/Passagem/PassagensService.cs
@@ -0,0 +1,24 @@
+﻿
+namespace go_horse_voos_comerciais.Domain.Passagem
+{
+    public class PassagensService : IPassagensService
+    {
+
+        private readonly ApiGhvcDbContext _context;
+
+        public PassagensService(ApiGhvcDbContext context)
+        {
+            this._context = context;
+        }
+        public List<Passagens> GerarPassagens(long idReserva, int quantidadeAssentosDesejados)
+        {
+            List<Passagens> passagens = new List<Passagens>();
+            for (int i = 0; i < quantidadeAssentosDesejados; i++)
+            {
+                passagens.Add(new Passagens(idReserva));
+            }
+
+            return passagens;
+        }
+    }
+}
